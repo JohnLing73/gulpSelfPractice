ec211fd3aca62bb929e75fc7206fcd8487ff6c11
JohnLing73 <dudulala73@gmail.com>
2021-03-23 23:50:45 +0800

    add

--- gulpfile.js
+++ gulpfile.js
@@ -37,8 +37,25 @@ exports.doB = parallel(funcA, funcB);//任務並行
 
 //=================3. 打包 src / dest
 
+// 打包JS
 function moveFile(){
     return src('./js/main.js').pipe(dest('output'));
 }
 
-exports.cp = moveFile;//輸出任務
+//打包CSS
+function moveCSSFile(){
+    return src('./css/style.css').pipe(dest('output/css'));
+}
+
+
+// exports.cp = moveFile;//輸出任務
+
+
+//=========== 4 watch 監看
+
+function watchTask(){
+    watch('js/main.js', moveFile);//當main.js變動時，會執行moveFile ||watch下，前面有輸出過的任務沒辦法被私有找到，需要註解掉前面的
+    watch('css/style.css',moveCSSFile);
+}
+
+exports.doWatch = watchTask;//
